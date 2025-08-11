0382ae8d0bec9a6c4a530deeb04dc50aaecc7332
pedro castro <falecompedroac@gmail.com>
2025-08-11 11:31:00 +0100

    root endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get()
+  root() {
+    return {
+      status: 'online',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+      version: process.env.npm_package_version,
+      message: 'Welcome to the NIddle API!',
+    };
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FacultiesModule } from './faculties/faculties.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [DatabaseModule, FacultiesModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
