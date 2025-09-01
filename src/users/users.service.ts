c585550a85a8f454dfd5a3612d65be29f05aa90f
Pedro Castro <falecompedroac@gmail.com>
2025-09-01 15:03:50 +0100

    get by id endpoint

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -12,5 +12,13 @@ export class UsersService {
 
   findAll(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
   }
 }
