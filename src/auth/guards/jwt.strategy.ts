42a136c41e59e3bcb3dbec55d38c695e951e7712
Pedro Castro <falecompedroac@gmail.com>
2025-10-02 19:57:50 +0100

    throwing an error if jwt secret is not set

--- src/auth/guards/jwt.strategy.ts
+++ src/auth/guards/jwt.strategy.ts
@@ -6,7 +6,8 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
-    const secret = configService.get<string>('JWT_SECRET')!;
+    const secret = configService.get<string>('JWT_SECRET');
+    if (!secret) throw new Error('JWT_SECRET is not set');
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
