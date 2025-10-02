951b8fe6c08f7ee649e55c4f2b0c8424699592e4
Pedro Castro <falecompedroac@gmail.com>
2025-10-02 20:19:42 +0100

    improves faculties endpoints documentation

--- src/faculties/faculties.controller.ts
+++ src/faculties/faculties.controller.ts
@@ -15,23 +15,31 @@ import { CreateFacultyDto } from './dto/create-faculty.dto';
 import { UpdateFacultyDto } from './dto/update-faculty.dto';
 import { Faculty } from './faculty.entity';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('faculties')
 export class FacultiesController {
   constructor(private readonly facultiesService: FacultiesService) {}
 
+  @ApiOperation({ summary: 'Get all faculties' })
+  @ApiResponse({ status: 200, description: 'List of faculties returned.' })
   @Get()
   findAll(): Promise<Faculty[]> {
     return this.facultiesService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get faculty by ID' })
+  @ApiResponse({ status: 200, description: 'Faculty found.' })
+  @ApiResponse({ status: 404, description: 'Faculty not found.' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number): Promise<Faculty> {
     return this.facultiesService.findOne(id);
   }
 
   @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: 'Create a new faculty' })
+  @ApiResponse({ status: 201, description: 'Faculty created.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   @UseGuards(JwtAuthGuard)
   @Post()
   create(
@@ -41,6 +49,10 @@ export class FacultiesController {
   }
 
   @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: 'Update a faculty by ID' })
+  @ApiResponse({ status: 200, description: 'Faculty updated.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiResponse({ status: 404, description: 'Faculty not found.' })
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
@@ -51,6 +63,10 @@ export class FacultiesController {
   }
 
   @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: 'Delete a faculty by ID' })
+  @ApiResponse({ status: 200, description: 'Faculty deleted.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiResponse({ status: 404, description: 'Faculty not found.' })
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number): Promise<Faculty> {
