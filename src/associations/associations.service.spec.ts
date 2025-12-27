db83a773cccfcd2ce0c7983eb58c4472ec617e60
danielramaa <danie.morais.rama@gmail.com>
2025-12-27 17:58:44 +0000

    checks failed (fixed(i think))

--- src/associations/associations.service.spec.ts
+++ src/associations/associations.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssociationsService } from './associations.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Association } from './entities/association.entity';
 
 describe('AssociationsService', () => {
   let service: AssociationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssociationsService],
+      providers: [
+        AssociationsService,
+        {
+          provide: getRepositoryToken(Association),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AssociationsService>(AssociationsService);
