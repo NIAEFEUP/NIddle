341a668f8e6f195b7c28f3d151a2478563291d46
Pedro Castro <falecompedroac@gmail.com>
2025-12-27 00:13:38 +0000

    create calendar dto

--- src/calendars/dto/create-calendar.dto.ts
+++ src/calendars/dto/create-calendar.dto.ts
@@ -1,1 +1,27 @@
-export class CreateCalendarDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsNumber,
+  IsArray,
+  ArrayUnique,
+} from 'class-validator';
+
+export class CreateCalendarDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  facultyId: number;
+
+  @IsArray()
+  @ArrayUnique()
+  @IsNumber({}, { each: true })
+  @IsOptional()
+  eventIds?: number[];
+}
