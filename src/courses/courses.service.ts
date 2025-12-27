bec7f8168235c0220c5b3cf469bceee9f9bbe929
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-12-27 16:41:11 +0000

    feat: added courses service with create function

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from './entities/course.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CoursesService {
+  constructor(
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>,
+  ) {}
+
+  create(createCourseDto: CreateCourseDto): Promise<Course> {
+    return this.courseRepository.manager.transaction(async (manager) => {
+      const courseRepo = manager.getRepository(Course);
+      const course = courseRepo.create(createCourseDto);
+      return courseRepo.save(course);
+    });
+  }
+
+  findAll() {
+    return `This action returns all courses`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} course`;
+  }
+
+  update(id: number, updateCourseDto: UpdateCourseDto) {
+    return `This action updates a #${id} course`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} course`;
+  }
+}
