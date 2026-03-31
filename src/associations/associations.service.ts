42e986424dfb5760e8beeaf1fd28196fc0c85d30
Pedro Castro <falecompedroac@gmail.com>
2026-03-31 14:59:16 +0100

    updated association servcie to follow project standards

--- src/associations/associations.service.ts
+++ src/associations/associations.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { Course } from "@/courses/entities/course.entity";
+import { Faculty } from "@/faculties/entities/faculty.entity";
+import { User } from "@/users/entities/user.entity";
 import { CreateAssociationDto } from "./dto/create-association.dto";
 import { UpdateAssociationDto } from "./dto/update-association.dto";
 import { Association } from "./entities/association.entity";
@@ -10,16 +13,40 @@ export class AssociationsService {
   constructor(
     @InjectRepository(Association)
     private associationRepository: Repository<Association>,
+    @InjectRepository(Faculty)
+    private facultyRepository: Repository<Faculty>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>,
   ) {}
 
-  create(createAssociationDto: CreateAssociationDto): Promise<Association> {
-    const association = this.associationRepository.create(
-      createAssociationDto as Partial<Association>,
-    );
+  async create(
+    createAssociationDto: CreateAssociationDto,
+  ): Promise<Association> {
+    const { facultyId, userId, courseId, ...associationData } =
+      createAssociationDto;
+
+    const association = this.associationRepository.create(associationData);
+
+    association.faculty = await this.facultyRepository.findOneByOrFail({
+      id: facultyId,
+    });
+
+    association.user = await this.userRepository.findOneByOrFail({
+      id: userId,
+    });
+
+    if (courseId !== undefined && courseId !== null) {
+      association.course = await this.courseRepository.findOneByOrFail({
+        id: courseId,
+      });
+    }
+
     return this.associationRepository.save(association);
   }
 
-  findAll(facultyId?: number) {
+  findAll(facultyId?: number): Promise<Association[]> {
     if (facultyId) {
       return this.associationRepository.find({
         where: { faculty: { id: facultyId } },
@@ -31,18 +58,53 @@ export class AssociationsService {
     });
   }
 
-  findOne(id: number) {
-    return this.associationRepository.findOne({
+  findOne(id: number): Promise<Association> {
+    return this.associationRepository.findOneOrFail({
       where: { id },
       relations: ["faculty", "user"],
     });
   }
 
-  update(id: number, updateAssociationDto: UpdateAssociationDto) {
-    return this.associationRepository.update(id, updateAssociationDto);
+  async update(
+    id: number,
+    updateAssociationDto: UpdateAssociationDto,
+  ): Promise<Association> {
+    const { facultyId, userId, courseId, ...associationData } =
+      updateAssociationDto;
+
+    const association = await this.associationRepository.findOneOrFail({
+      where: { id },
+      relations: ["faculty", "user", "course"],
+    });
+
+    this.associationRepository.merge(association, associationData);
+
+    if (facultyId !== undefined) {
+      association.faculty = await this.facultyRepository.findOneByOrFail({
+        id: facultyId,
+      });
+    }
+
+    if (userId !== undefined) {
+      association.user = await this.userRepository.findOneByOrFail({
+        id: userId,
+      });
+    }
+
+    if (courseId !== undefined) {
+      association.course = await this.courseRepository.findOneByOrFail({
+        id: courseId,
+      });
+    }
+
+    return this.associationRepository.save(association);
   }
 
-  remove(id: number) {
-    return this.associationRepository.delete(id);
+  async remove(id: number): Promise<Association> {
+    const association = await this.associationRepository.findOneByOrFail({
+      id,
+    });
+    await this.associationRepository.delete(id);
+    return association;
   }
 }
