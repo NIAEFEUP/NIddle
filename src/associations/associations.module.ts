8c7b17ba62ca6fb4971e5ffdc768c15544ca7431
Pedro Castro <falecompedroac@gmail.com>
2026-03-31 14:57:41 +0100

    implemented create association dto

--- src/associations/associations.module.ts
+++ src/associations/associations.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Course } from "@/courses/entities/course.entity";
+import { Faculty } from "@/faculties/entities/faculty.entity";
+import { User } from "@/users/entities/user.entity";
 import { AssociationsController } from "./associations.controller";
 import { AssociationsService } from "./associations.service";
 import { Association } from "./entities/association.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Association])],
+  imports: [TypeOrmModule.forFeature([Association, Faculty, User, Course])],
   controllers: [AssociationsController],
   providers: [AssociationsService],
   exports: [AssociationsService],
