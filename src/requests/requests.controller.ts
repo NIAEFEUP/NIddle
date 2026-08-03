81e367842de446b2641af63b923fd1d6020440a0
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-08-03 21:34:55 +0100

    feat: added create request route (working)

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, Post, Req, UseGuards, UseInterceptors, ValidationPipe } from "@nestjs/common";
 import { RequestsService } from "./requests.service";
+import { CreateRequestDto } from "./dto/create-request.dto";
+import { JwtAuthGuard } from "@/auth/guards/jwt-auth.guard";
+import { Request } from "@/requests/entities/request.entity";
+import { User } from "@/users/entities/user.entity";
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller("requests")
 export class RequestsController {
   constructor(private readonly requestsService: RequestsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(
+    @Body(ValidationPipe) createRequestDto: CreateRequestDto,
+    @Req() req : { user : User },
+  ): Promise<Request> {
+    return this.requestsService.create(createRequestDto, req.user);
+  }
 }
