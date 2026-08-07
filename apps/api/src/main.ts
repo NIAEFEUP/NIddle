19e322a7168bf3d0a5db57e97e0ba26445819b25
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-08-07 21:45:43 +0100

    feat: added whitelist and forbidNonWhiteList properties to globas validationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,10 +4,17 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { EntityNotFoundFilter } from "./filters/entity-not-found.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    
+  }));
+  app.useGlobalFilters(new EntityNotFoundFilter());
 
   const port = process.env.PORT ?? 3001;
   const config = new DocumentBuilder()
