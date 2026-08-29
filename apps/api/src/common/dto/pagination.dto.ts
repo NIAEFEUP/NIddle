8e70ff1f8f8365d1d7e8bc5099bde08c8bcf2b6a
goncalopinto1 <goncalo.luis.pinto@gmail.com>
2026-08-29 23:31:17 +0100

    fix: fix swagger documentation about pagination

--- apps/api/src/common/dto/pagination.dto.ts
+++ apps/api/src/common/dto/pagination.dto.ts
@@ -1,17 +1,28 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsInt, IsOptional, Max, Min } from "class-validator";
 
 export class PaginationDto {
-    @IsOptional()
-    @IsInt()
-    @Min(1)
-    @Type(() => Number)
-    page: number = 1;
+  /**
+   * The page of the data we want to search'
+   * @example 2
+   */
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  page: number = 1;
 
-    @IsOptional()
-    @IsInt()
-    @Min(1)
-    @Max(100)
-    @Type(() => Number)
-    limit: number = 10;
+  /**
+   * The number of rows to be returned per page
+   * @example 10
+   */
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  @Type(() => Number)
+  limit: number = 10;
 }
