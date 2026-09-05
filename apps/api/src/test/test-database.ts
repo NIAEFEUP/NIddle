83c5cf3feedd26a8a04a944efcd614454834b24c
Pedro Castro <falecompedroac@gmail.com>
2026-09-05 19:30:53 +0100

    fix(test): fix entity metadata and synchronize spec for Postgres

--- apps/api/src/test/test-database.ts
+++ apps/api/src/test/test-database.ts
@@ -1,4 +1,12 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Association } from "@/associations/entities/association.entity";
+import { Course } from "@/courses/entities/course.entity";
+import { Event } from "@/events/entities/event.entity";
+import { Faculty } from "@/faculties/entities/faculty.entity";
+import { Request } from "@/requests/entities/request.entity";
+import { Schedule } from "@/services/entity/schedule.entity";
+import { Service } from "@/services/entity/service.entity";
+import { User } from "@/users/entities/user.entity";
 
 export function getTestDatabaseOptions(): TypeOrmModuleOptions {
   return {
@@ -8,7 +16,16 @@ export function getTestDatabaseOptions(): TypeOrmModuleOptions {
     username: process.env.TEST_DATABASE_USER,
     password: process.env.TEST_DATABASE_PASSWORD,
     database: process.env.TEST_DATABASE_NAME,
-    autoLoadEntities: true,
+    entities: [
+      Association,
+      Course,
+      Event,
+      Faculty,
+      Request,
+      Schedule,
+      Service,
+      User,
+    ],
     synchronize: true,
   };
 }
